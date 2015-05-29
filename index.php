f1211c8e17840734ca3f91b73ce745b0eb9b9261
zephox <alexiakab@hotmail.com>
2015-05-29 10:54:40 +0200

    index fixed

--- index.php
+++ index.php
@@ -1,17 +1,48 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
-</html>
+<?php
+// array for JSON response
+$response = array();
+ 
+// include db connect class
+require_once __DIR__ . '/db_connect.php';
+ 
+// connecting to db
+$db = new DB_CONNECT();
+if($_GET["ID"] !== "")
+{
+    $test = $_GET["ID"];
+    $type = $_GET["Type"];
+    $result = mysql_query("SELECT *FROM $type WHERE ID =$test") or die(mysql_error());
+
+    if (mysql_num_rows($result) > 0) {
+        $response[$type] = array();
+        while ($row = mysql_fetch_array($result)) {
+            // temp user array
+            $product = array();
+            if ($type === "Leraar")
+            {
+                $product['id'] = $row[0];
+                $product['name'] = $row[1];
+            }
+            if ($type === "Vak")
+            {
+                $product['id'] = $row[0];
+                $product['name'] = $row[1];
+            }
+            if ($type === "Lokaal")
+            {
+                $product['id'] = $row[0];
+                $product['locatie'] = $row[1];
+                $product['functie'] = $row[2];
+            }
+            array_push($response[$type], $product);
+        }
+        echo json_encode($response);
+    } 
+    else 
+    {
+        $response["success"] = 0;
+        $response["message"] = "No products found";
+        echo json_encode($response);
+    }
+}
+?>
