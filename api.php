c3f5ae3d466585d95a10f569191b4771b178699b
Goz3rr <wisse.aarts@zonnet.nl>
2015-06-04 13:05:01 +0200

    oeps

--- api.php
+++ api.php
@@ -26,15 +26,29 @@ if($db->error !== null) {
         $stmt = $db->con->prepare("SELECT Leraar.Naam, (SELECT GROUP_CONCAT(Vak.Naam) FROM Vak, Vakken WHERE Vakken.Leraar = Leraren.Leraar AND Vak.ID = Vakken.Vak) AS GegevenVakken FROM Leraar, Leraren WHERE Leraren.QRCode = ? AND Leraar.ID = Leraren.Leraar");
         $stmt->bind_param("i", $id);
         $stmt->execute();
-        $result = $stmt->get_result();
-        $response["leraren"] = $result->fetch_array(MYSQLI_NUM);
+        $stmt->bind_result($naam, $vakken);
+
+        $leraren = array();
+        while ($stmt->fetch()) {
+            array_push($leraren, array($naam, $vakken));
+        }
+
+        if(count($leraren) > 0) $response["leraren"] = $leraren
+
         $stmt->close();
 
         $stmt = $db->con->prepare("SELECT Lokaal.Nummer, Lokaal.Functie FROM Lokaal, Lokalen WHERE Lokalen.QRCode = ? AND Lokaal.ID = Lokalen.Lokaal");
         $stmt->bind_param("i", $id);
         $stmt->execute();
-        $result = $stmt->get_result();
-        $response["lokalen"] = $result->fetch_array(MYSQLI_NUM);
+        $stmt->bind_result($nummer, $functie);
+
+        $lokalen = array();
+        while ($stmt->fetch()) {
+            array_push($lokalen, array($nummer, $functie));
+        }
+
+        if(count($lokalen) > 0) $response["lokalen"] = $lokalen
+
         $stmt->close();
 
         $response["success"] = true;
