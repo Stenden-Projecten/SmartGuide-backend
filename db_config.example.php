92aedca1ef4e0171567888e73ce40aa1fad9d28c
Goz3rr <wisse.aarts@zonnet.nl>
2015-06-04 10:57:33 +0200

    config toegevoegd aan gitignore

---
+++ db_config.example.php
@@ -0,0 +1,11 @@
+<?php
+ 
+/*
+ * All database connection variables
+ */
+ 
+define('DB_USER', "smartguide"); // db user
+define('DB_PASSWORD', ""); // db password (mention your db password here)
+define('DB_DATABASE', "smartguide"); // database name
+define('DB_SERVER', "newton.kuubstudios.com"); // db server
+?>
