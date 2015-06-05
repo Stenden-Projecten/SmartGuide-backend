caa3d82b838e9e0db587f721dc376d62de8b7c93
Goz3rr <wisse.aarts@zonnet.nl>
2015-06-05 12:03:27 +0200

    error toegevoegd

--- api.php
+++ api.php
@@ -7,52 +7,57 @@ require_once(__DIR__ . '/db_connect.php');
 $db = new DB_CONNECT();
 
 if($db->error !== null) {
-    $response["error"] = $db->error;
+	$response["error"] = $db->error;
 } else {
-    $id = isset($_GET["ID"]) ? $_GET["ID"] : null;
+	$id = isset($_GET["ID"]) ? $_GET["ID"] : null;
 
-    if($id == null) {
-        $response["error"] = "ID invoer verplicht";
-    } else {
-        $stmt = $db->con->prepare("SELECT QRCode.Tekst FROM QRCode WHERE ID = ?");
-        $stmt->bind_param("i", $id);
-        $stmt->execute();
-        $stmt->bind_result($tekst);
-        $stmt->fetch();
-        $stmt->close();
+	if($id == null) {
+		$response["error"] = "ID invoer verplicht";
+	} else {
+		$stmt = $db->con->prepare("SELECT QRCode.Tekst FROM QRCode WHERE ID = ?");
+		$stmt->bind_param("i", $id);
+		$stmt->execute();
+		$stmt->bind_result($tekst);
+		$stmt->fetch();
+		$rows = $stmt->num_rows;
+		$stmt->close();
 
-        $response["tekst"] = $tekst;
+		if($rows == 0) {
+			$response["error"] = "geen resultaten";
+		} else {
+			$response["tekst"] = $tekst;
 
-        $stmt = $db->con->prepare("SELECT Leraar.Naam, (SELECT GROUP_CONCAT(Vak.Naam) FROM Vak, Vakken WHERE Vakken.Leraar = Leraren.Leraar AND Vak.ID = Vakken.Vak) AS GegevenVakken FROM Leraar, Leraren WHERE Leraren.QRCode = ? AND Leraar.ID = Leraren.Leraar");
-        $stmt->bind_param("i", $id);
-        $stmt->execute();
-        $stmt->bind_result($naam, $vakken);
+			$stmt = $db->con->prepare("SELECT Leraar.Naam, (SELECT GROUP_CONCAT(Vak.Naam) FROM Vak, Vakken WHERE Vakken.Leraar = Leraren.Leraar AND Vak.ID = Vakken.Vak) AS GegevenVakken FROM Leraar, Leraren WHERE Leraren.QRCode = ? AND Leraar.ID = Leraren.Leraar");
+			$stmt->bind_param("i", $id);
+			$stmt->execute();
+			$stmt->bind_result($naam, $vakken);
 
-        $leraren = array();
-        while ($stmt->fetch()) {
-            $leraren[$naam] = $vakken;
-        }
+			$leraren = array();
+			while ($stmt->fetch()) {
+				$leraren[$naam] = $vakken;
+			}
 
-        if(count($leraren) > 0) $response["leraren"] = $leraren;
+			if(count($leraren) > 0) $response["leraren"] = $leraren;
 
-        $stmt->close();
+			$stmt->close();
 
-        $stmt = $db->con->prepare("SELECT Lokaal.Nummer, Lokaal.Functie FROM Lokaal, Lokalen WHERE Lokalen.QRCode = ? AND Lokaal.ID = Lokalen.Lokaal");
-        $stmt->bind_param("i", $id);
-        $stmt->execute();
-        $stmt->bind_result($nummer, $functie);
+			$stmt = $db->con->prepare("SELECT Lokaal.Nummer, Lokaal.Functie FROM Lokaal, Lokalen WHERE Lokalen.QRCode = ? AND Lokaal.ID = Lokalen.Lokaal");
+			$stmt->bind_param("i", $id);
+			$stmt->execute();
+			$stmt->bind_result($nummer, $functie);
 
-        $lokalen = array();
-        while ($stmt->fetch()) {
-            $lokalen[$nummer] = $functie;
-        }
+			$lokalen = array();
+			while ($stmt->fetch()) {
+				$lokalen[$nummer] = $functie;
+			}
 
-        if(count($lokalen) > 0) $response["lokalen"] = $lokalen;
+			if(count($lokalen) > 0) $response["lokalen"] = $lokalen;
 
-        $stmt->close();
+			$stmt->close();
 
-        $response["success"] = true;
-    }
+			$response["success"] = true;
+		}
+	}
 }
 
 header("Content-type: application/json");
